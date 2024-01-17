30097d41394b61573c6d9e915d36c913431ebb72
Roland HUON <huonroland91@gmail.com>
2024-01-17 12:08:52 +0100

    🔥 change page name

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,23 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+export default{
+  name:'App',
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/playlist">Playlist</RouterLink>
       </nav>
-    </div>
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
   width: 100%;
   font-size: 12px;
--- src/views/PlaylistView.vue
+++ src/views/PlaylistView.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-  
+  <div class="about">
+    <h1>This is a playlist page</h1>
+  </div>
 </template>
 
 <style scoped>
